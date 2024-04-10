ead265c3bbfb7535292629c67c0a33418ed4937a
iridero <jcgarza_mx@hotmail.com>
2024-04-10 15:27:10 -0600

    Creacion formulario addHabitat

--- src/app/components/add-habitat/add-habitat.component.ts
+++ src/app/components/add-habitat/add-habitat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Habitat } from '../../models/habitat.model';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-add-habitat',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddHabitatComponent implements OnInit {
 
-  constructor() { }
+  newHabitat:Habitat = new Habitat(0,'','');
+
+  constructor(private localStorageService:LocalStorageService) { }
+
+  addHabitat(){
+    this.localStorageService.saveHabitat(this.newHabitat);
+    this.newHabitat = new Habitat(0,'','')
+  }
 
   ngOnInit() {
   }
